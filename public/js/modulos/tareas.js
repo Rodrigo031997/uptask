d058232810e7430ca2221b76b155ef1327181d29
alaguna <rodrigoalagunamoreno@gmail.com>
2020-05-11 00:38:20 -0500

    Carpeta 20 Mostrando el avance del proyecto

--- public/js/modulos/tareas.js
+++ public/js/modulos/tareas.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import Swal from 'sweetalert2';
+import {actualizarAvance} from '../funciones/avance';
 
 const tareas = document.querySelector('.listado-pendientes');
 
@@ -17,7 +18,6 @@ if(tareas) {
                 .then(function(respuesta){
                     if(respuesta.status === 200){
                         icono.classList.toggle('completo');
-
                         actualizarAvance();
                     }
                 })
